5a276bca42d98972d6655d95ad91666277eece86
yuyilei <yyl1352091742@gmail.com>
2018-06-04 20:43:03 +0800

    pipe

--- unixhomework/p14.c
+++ unixhomework/p14.c
@@ -38,35 +38,38 @@ int WAIT_CHILD(void) {
     return res;
 }
 
-void TELL_CHILD(char signal) {
+void TELL_CHILD(int signals[2]) {
     /***
      * 父进程唤醒子进程
      */
-    if (write(pfd1[1], &signal, 1) != 1) {
+    if (write(pfd1[1], signals, 2*sizeof(int)) != 2*sizeof(int) ) {
         fprintf(stderr, "write error");
     }
     fflush(stdout);
 }
 
-char WAIT_PARENT(void) {
+void WAIT_PARENT(int signals[2]) {
     /***
      * 子进程等待父进程通知
      */
     char c;
-    while (read(pfd1[0], &c, 1) != 1) {
+    while (read(pfd1[0], signals, 2*sizeof(int)) != 2*sizeof(int)) {
         fprintf(stderr, "read error");
     }
 
-    if (c != 'p' && c != 'q' ) {
+    if (signals[0] != 0 && signals[0] != 1 && signals[0] != 2  ) {
         fprintf(stderr, "WAIT_PARENT: incorrect data");
         exit(1);
     }
-    return c;
 }
 
-int generate_random() {
+
+int generate_random(int s, int e) {
+    /*
+     * 生成s到e的随机数
+     */
     srand((unsigned int)time(NULL));
-    int num = rand() % 100 + 1;
+    int num = rand() % ( e - s + 1 ) + s;
     return num ;
 }
 
@@ -82,35 +85,48 @@ int main(int argc, char** argv) {
 
     else if (pid > 0) {
         // 父进程
-        int num = generate_random();
+        int num = generate_random(1,100);
         int num2 = 0;
         while ( num != num2 ) {
             num2 = WAIT_CHILD();                                  // 获取子进程中猜的数字
+            int signals[2] = {0} ;
             if ( num2 > num ) {
                 printf("In father process: 猜大了!\n") ;
-                TELL_CHILD('p');                                 // p 表示继续猜
+                signals[0] = 1 ;
             }
             else if ( num2 < num ) {
                 printf("In father process: 猜小了!\n") ;
-                TELL_CHILD('p');                                 // p 表示继续猜
+                signals[0] = 2 ;
             }
             else {
                 printf("In father process: 猜对了!\n") ;
-                TELL_CHILD('q');                                 // 写入q, 停止猜
+                signals[0] = 0 ;
             }
+            signals[1] = num2 ;
+            // signal[0] 表示猜大了还是猜小了，signals[1] 是上一轮的猜的数
+            TELL_CHILD(signals);
         }
         printf("In fahter process: 随机生成的数字是:%d\n",num) ;
     }
     else {
         // 子进程
         int num2;
-        char flag;
+        int signals[2];
+        int s = 1, e = 100;
         do {
-            printf("In child process: 输入一个1到100的数\n") ;
-            scanf("%d",&num2) ;
+            printf("In child process: 猜一个%d到%d的数\n",s,e) ;
+			int sleep_time = generate_random(1,3) ;
+            sleep(sleep_time) ;
+            num2 = generate_random(s,e) ;                       // 生成随机数
+            printf("In child process: 猜这个数为%d\n",num2) ;
             TELL_PARENT(num2) ;                                // 把猜的数字写入管道
-            flag = WAIT_PARENT();
-        } while ( flag == 'p' ) ;
+            WAIT_PARENT(signals);
+            // 根据上一轮猜的结果改变范围的大小
+            if ( signals[0] == 1 )
+                e = signals[1] - 1 ;
+            else if ( signals[0] == 2 )
+                s = signals[1] + 1 ;
+        } while ( signals[0] != 0 ) ;
     }
     return 0;
 }
